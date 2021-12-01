eae9c41522ebf9982fc3c4ac5ddb7a581255366f
Rachel S <thelivedish@gmail.com>
2021-11-30 16:34:39 -0800

    added app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./home";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Keisha's Link page</p>
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div class="flex flex-col ">
+      <div class="my-8 border-b-2 border-purple-600">Amazon</div>
+      <div class="my-8 border-b-2 border-purple-600">Black Book Store</div>
+      <div class="my-8">Goodreads</div>
+      <div class="my-8">Facebook</div>
+    </div>
+  );
+}
+
+export default Home;
