292e273591b54e0362d503cd1497f99c9e3d13ab
Rachel S <thelivedish@gmail.com>
2021-12-06 22:30:24 -0800

    social set

--- src/Footer.js
+++ src/Footer.js
@@ -6,11 +6,11 @@ function Footer() {
     <div class="h-16">
       <h1 class="text-4xl"> Follow me</h1>
       <div class="h-12 flex flex-row gap-4 justify-center">
-        <a href="http://www.instagram.com">
+        <a href="https://www.instagram.com/keisha_writenow_allen/">
           {" "}
           <FaInstagram class="text-2xl" />{" "}
         </a>
-        <a href="http://www.facebook.com">
+        <a href="https://www.facebook.com/KeishaWriteNowAllen">
           {" "}
           <FaFacebook class="text-2xl" />{" "}
         </a>
